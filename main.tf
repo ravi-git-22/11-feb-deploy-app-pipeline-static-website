4657f3948bdb830a32f070d818119193f3a9cc72
ravi-git-22 <ravikiran.thames@outlook.com>
2023-02-11 13:37:40 +0000

    commit-4:modified storage acc name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "static_website_rg" {
 }
 
 resource "azurerm_storage_account" "static_website_storage" {
-  name                     = "deploy-web-app-tf-11FEB"
+  name                     = "deploy-web-app-tf-11feb"
   resource_group_name      = azurerm_resource_group.static_website_rg.name
   location                 = azurerm_resource_group.static_website_rg.location
   account_tier             = "Standard"
