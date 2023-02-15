75b6f39dc813253b172bde97712de850eddc06fd
ravi-git-22 <ravikiran.thames@outlook.com>
2023-02-15 01:11:43 +0000

    commit-37:trying with random stor acc name and yaml pipeline

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
+resource "random_string" "myrandom" {
+  length = 16
+  upper = false 
+  special = false
+}
 resource "azurerm_resource_group" "static_website_rg" {
-  name     = "static-website-rg2"
+  name     = "static-website-rg1"
   location = var.location
 }
 
-resource "azurerm_storage_account" "static_website_storage" {
-  name                     = "webappon13feb"
+resource "azurerm_storage_account" "mysa" {
+  name                     = "mysa${random_string.myrandom.id}"
   resource_group_name      = azurerm_resource_group.static_website_rg.name
   location                 = var.location
   account_tier             = "Standard"
